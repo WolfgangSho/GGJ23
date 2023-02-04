040082013b0b6fa531e19bbead33a85cc92637bf
WolfgangSho <htspencer88@gmail.com>
2023-02-04 08:00:12 +0000

    Devil now does damage
    Takes away from plant XP. Plants delevel as expected.

--- Assets/Scripts/DevilMovement.cs
+++ Assets/Scripts/DevilMovement.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class DevilMovement : MonoBehaviour
 {
+    public GameObject PlantsGO;
+    
     public float moveAmount;
 
     public float minTimeToMove, maxTimeToMove;
+
+    public int damageAmount;
+
+    public float damageTick;
 
     public int centreIndex;
 
@@ -14,32 +20,34 @@ public class DevilMovement : MonoBehaviour
 
     int devilIndex;
 
-    float timer;
+    float moveTimer, damageTimer;
+
+    PlantManager pm;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        pm = PlantsGO.GetComponent<PlantManager>();
         devilIndex = centreIndex;
         UpdatePosition();
-        timer = 0;
+        moveTimer = 0;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timer > 0)
+        if(moveTimer > 0)
         {
-            timer -= Time.deltaTime;
+            moveTimer -= Time.deltaTime;
 
-            if(timer <= 0)
+            if(moveTimer <= 0)
             {
-                timer = 0;
+                moveTimer = 0;
             }
         }
-        else if (timer == 0)
+        else if (moveTimer == 0)
         {
             int desiredIndex = Random.Range(0,maxIndex+1);
 
@@ -56,7 +64,22 @@ public class DevilMovement : MonoBehaviour
 
             UpdatePosition();
 
-            timer = Random.Range(minTimeToMove,maxTimeToMove);
+            moveTimer = Random.Range(minTimeToMove,maxTimeToMove);
+        }
+
+        if(damageTimer == 0)
+        {
+            pm.AlterPlantXP(devilIndex,-damageAmount);
+            damageTimer = damageTick;
+        }
+        else
+        {
+            damageTimer -= Time.deltaTime;
+
+            if(damageTimer < 0)
+            {
+                damageTimer = 0;
+            }
         }
     }
 
